f577f5e5ad04345da95d51fe6aafa41335996111
Oliver King <11923466+OllyK@users.noreply.github.com>
2024-05-17 00:30:25 +0100

    Reformats augmentations and model files

--- detector/data/augmentations.py
+++ detector/data/augmentations.py
@@ -1,34 +1,44 @@
 import albumentations as A
-from albumentations.pytorch.transforms  import ToTensorV2
+from albumentations.pytorch.transforms import ToTensorV2
+
 
 def get_transform(train):
+    """
+    Returns the data augmentation transformations for training or testing.
+
+    Parameters:
+    - train (bool): If True, returns the transformations for training. If False, returns the transformations for testing.
+
+    Returns:
+    - A.Compose: The composed transformations.
+    """
     if train:
         train_transform = [
-            #A.ToFloat(),
-            A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=30, p=0.3),
+            A.ShiftScaleRotate(
+                shift_limit=0.1, scale_limit=0.1, rotate_limit=30, p=0.3
+            ),
             A.HorizontalFlip(p=0.5),
             A.VerticalFlip(p=0.5),
             A.RandomRotate90(p=0.5),
             A.OneOf(
                 [
                     A.RandomGamma(p=0.7),
-                    A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, p=0.7),
+                    A.RandomBrightnessContrast(
+                        brightness_limit=0.3, contrast_limit=0.3, p=0.7
+                    ),
                     A.HueSaturationValue(p=0.7),
                 ],
-                p=0.7
+                p=0.7,
             ),
-             A.OneOf(
-                [
-                    A.Sharpen(p=0.3),
-                    A.Blur(blur_limit=3, p=0.7)
-                ],
+            A.OneOf(
+                [A.Sharpen(p=0.3), A.Blur(blur_limit=3, p=0.7)],
                 p=0.7,
             ),
             ToTensorV2(),
         ]
     else:
-        train_transform = [
-            ToTensorV2()
-        ]
-    return A.Compose(train_transform, bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_labels']))
-
+        train_transform = [ToTensorV2()]
+    return A.Compose(
+        train_transform,
+        bbox_params=A.BboxParams(format="pascal_voc", label_fields=["class_labels"]),
+    )
