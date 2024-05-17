ca9bf809edaed2c818ced5c1633a91c6cb8c6268
Oliver King <11923466+OllyK@users.noreply.github.com>
2024-05-17 12:26:53 +0100

    Reformats dataloader and datasets files

--- detector/data/dataloaders.py
+++ detector/data/dataloaders.py
@@ -1,30 +1,45 @@
 import logging
 from types import SimpleNamespace
-
 import torch
 from torch.utils.data import DataLoader, Subset
 import numpy as np
-
 from detector.data.augmentations import get_transform
 from detector.data.datasets import ZooniverseXtalDropDataset
 from utilities import base_data_utils as utils
+
 import utilities.config as cfg
 
 
 def get_train_val_dataloaders(
     data_dir, settings: SimpleNamespace, fix_seed: bool = False
 ):
+    """
+    Get training and validation data loaders.
+
+    Args:
+        data_dir (str): Directory path to the data.
+        settings (SimpleNamespace): Settings object containing configuration parameters.
+        fix_seed (bool, optional): Whether to fix the random seed. Defaults to False.
+
+    Returns:
+        tuple: A tuple containing the training and validation data loaders.
+    """
     training_set_prop = settings.training_set_proportion
     batch_size = utils.get_batch_size(settings)
     labels = settings.class_names
-    # use our dataset and defined transformations
-    full_training_dset = ZooniverseXtalDropDataset(data_dir, labels, get_transform(train=True))
+
+    # Use our dataset and defined transformations
+    full_training_dset = ZooniverseXtalDropDataset(
+        data_dir, labels, get_transform(train=True)
+    )
     full_validation_dset = ZooniverseXtalDropDataset(
         data_dir, labels, get_transform(train=False)
     )
-    # split the dataset in train and test set
+
+    # Split the dataset into train and validation sets
     dset_length = len(full_training_dset)
     logging.info(f"Full dataset has {dset_length} members.")
+
     # Set predictable split for testing
     if fix_seed:
         seed_val = settings.random_seed
@@ -33,15 +48,17 @@ def get_train_val_dataloaders(
         rand_generator.manual_seed(seed_val)
     else:
         rand_generator = None
+
     indices = torch.randperm(dset_length, generator=rand_generator).tolist()
     train_idx, validate_idx = np.split(indices, [int(dset_length * training_set_prop)])
     logging.info(
         f"Splitting into training dataset: {len(train_idx)} members, validation dataset: {len(validate_idx)} members."
     )
+
     training_dataset = Subset(full_training_dset, train_idx)
     validation_dataset = Subset(full_validation_dset, validate_idx)
 
-    # define training and validation data loaders
+    # Define training and validation data loaders
     training_dataloader = DataLoader(
         training_dataset,
         batch_size=batch_size,
--- detector/data/datasets.py
+++ detector/data/datasets.py
@@ -1,37 +1,59 @@
-import logging
 import os
-
 import cv2
 import numpy as np
 import torch
 
 
 class ZooniverseXtalDropDataset(torch.utils.data.Dataset):
+    """
+    Custom dataset class for Zooniverse Xtal Drop dataset.
+    """
+
     def __init__(self, root, labels, transforms=None):
+        """
+        Initialize the dataset.
+
+        Args:
+            root (str): Root directory of the dataset.
+            labels (list): List of class labels.
+            transforms (callable, optional): Optional transforms to be applied to the data.
+        """
         self.root = root
         self.transforms = transforms
-        # load all image files, sorting them to
-        # ensure that they are aligned
+
+        # Load all image files, sorting them to ensure alignment
         self.imgs = list(sorted(os.listdir(os.path.join(root, "images"))))
         self.masks = list(sorted(os.listdir(os.path.join(root, "masks"))))
+
+        # Create a class key dictionary
         self.class_key = {v: k + 1 for k, v in enumerate(labels)}
-        logging.info(f"Class key: {self.class_key}")
-        
+
     def __getitem__(self, idx):
-        empty_flag= False
-        # load images ad masks
+        """
+        Get the item at the given index.
+
+        Args:
+            idx (int): Index of the item.
+
+        Returns:
+            tuple: A tuple containing the image and its corresponding target.
+        """
+        empty_flag = False
+
+        # Create image and mask paths
         img_path = os.path.join(self.root, "images", self.imgs[idx])
         mask_path = os.path.join(self.root, "masks", self.masks[idx])
-        # load in the image
+
+        # Load the image
         img = cv2.imread(img_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.float32)
-        # load in the corresponding mask and class label list
+
+        # Load the mask file and class labels
         mask_file = np.load(mask_path)
         masks = list(mask_file["masks"].astype(int))
-#         masks = np.array(masks).astype(int)
         class_labels = list(mask_file["class_labels"])
-        
-        # get bounding box coordinates for each mask
+
+        # Get bounding box coordinates for each mask
         num_objs = len(masks)
         boxes = []
         for i in range(num_objs):
@@ -41,49 +63,67 @@ class ZooniverseXtalDropDataset(torch.utils.data.Dataset):
             ymin = np.min(pos[0])
             ymax = np.max(pos[0])
             boxes.append([xmin, ymin, xmax, ymax])
+
         if num_objs == 0:
             empty_flag = True
             area = [0]
             num_objs = 1
         else:
             boxes = np.array(boxes, dtype=np.float32)
-     
+
         if self.transforms is not None and not empty_flag:
-            transformed = self.transforms(image=img, masks=masks, bboxes=boxes, class_labels=class_labels)
-            img = transformed['image']
+            # Apply transforms to the image, masks, and bounding boxes
+            transformed = self.transforms(
+                image=img, masks=masks, bboxes=boxes, class_labels=class_labels
+            )
+            img = transformed["image"]
             img = self.normalise_image(img)
-            masks = transformed['masks']
-            boxes = transformed['bboxes']
-            class_labels = transformed['class_labels']
+            masks = transformed["masks"]
+            boxes = transformed["bboxes"]
+            class_labels = transformed["class_labels"]
             labels = [self.class_key.get(item) for item in class_labels]
             labels = torch.as_tensor(labels, dtype=torch.int64)
-        
+
         if empty_flag:
+            # Handle the case when there are no objects in the image
             labels = torch.as_tensor(np.array([0], dtype=int), dtype=torch.int64)
             img = torch.from_numpy(img.transpose(2, 0, 1))
             img = self.normalise_image(img)
             boxes = np.array([[0, 1, 2, 3]], dtype=np.float32)
-            masks = [torch.zeros(( img.shape[1], img.shape[2]), dtype=torch.uint8)]
+            masks = [torch.zeros((img.shape[1], img.shape[2]), dtype=torch.uint8)]
 
-        boxes = torch.as_tensor(boxes, dtype=torch.float64)    
+        boxes = torch.as_tensor(boxes, dtype=torch.float64)
         if not empty_flag:
-                area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+            area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
 
         target = {}
         target["boxes"] = boxes
         target["labels"] = labels
         target["masks"] = torch.stack(masks).byte()
         target["image_id"] = torch.tensor([idx])
-        target['area'] = torch.as_tensor(area, dtype=torch.float32)
+        target["area"] = torch.as_tensor(area, dtype=torch.float32)
         target["iscrowd"] = torch.zeros((num_objs,), dtype=torch.int64)
 
         return img, target
-    
+
     def normalise_image(self, img):
+        """
+        Normalize the image.
+
+        Args:
+            img (torch.tensor): Input image.
+
+        Returns:
+            torch.tensor: Normalized image.
+        """
         i_range = img.max() - img.min()
-        return (img - img.min())/i_range
+        return (img - img.min()) / i_range
 
     def __len__(self):
-        return len(self.imgs)
+        """
+        Get the length of the dataset.
 
-    
+        Returns:
+            int: Length of the dataset.
+        """
+        return len(self.imgs)
