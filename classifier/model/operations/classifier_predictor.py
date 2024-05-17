968aeedde86f1a12ebf0946ffafbc3cd9c2acfd5
Oliver King <11923466+OllyK@users.noreply.github.com>
2024-05-17 16:23:54 +0100

    Adds docstrings to classifier_predictor file

--- classifier/model/operations/classifier_predictor.py
+++ classifier/model/operations/classifier_predictor.py
@@ -1,18 +1,41 @@
 from pathlib import Path
 import logging
 import pandas as pd
-
 from tqdm import tqdm
-
 from classifier.model.model import create_model_on_device
 from classifier.data.dataloaders import get_prediction_dataloader
-import utilities.config as cfg
 import torch
-import torch.nn as nn
 from collections import defaultdict
+
+import utilities.config as cfg
+import torch.nn as nn
 
 
 class ImageFolderClassifier:
+    """
+    A class for predicting image classes using a trained model.
+
+    Args:
+        model_path (Path): Path to the trained model.
+        image_list (list): List of image paths to predict classes for.
+        img_size (int): Size of the input images.
+        device (str, optional): Device to run the prediction on (default is "0").
+
+    Attributes:
+        model_path (Path): Path to the trained model.
+        image_list (list): List of image paths to predict classes for.
+        img_size (int): Size of the input images.
+        device (str): Device to run the prediction on.
+        output_dict (defaultdict): Dictionary to store the prediction results.
+        model_struc_dict (dict): Dictionary containing the model structure.
+        model (nn.Module): Trained model.
+        class_names (list): List of class names.
+        idx_to_class (dict): Mapping of class indices to class names.
+        output_dir (Path): Output directory path.
+        class_csv_output_path (Path): Path to the output CSV file.
+
+    """
+
     def __init__(
         self, model_path: Path, image_list: list, img_size: int, device: str = "0"
     ):
@@ -36,6 +59,13 @@ class ImageFolderClassifier:
         self._load_in_model(model_path)
 
     def _load_in_model(self, model_path):
+        """
+        Load the trained model from the given path.
+
+        Args:
+            model_path (Path): Path to the trained model.
+
+        """
         logging.info(f"Loading in model from {model_path}")
         model_dict = self._load_model_dict(model_path)
         self.model_struc_dict = model_dict["model_struc_dict"]
@@ -50,18 +80,41 @@ class ImageFolderClassifier:
         del model_dict
 
     def run(self, output_dir):
+        """
+        Run the image classification and output the results.
+
+        Args:
+            output_dir (Path): Output directory path.
+
+        """
         self._make_output_dir(output_dir)
         self._predict_classes()
         self._output_csv()
 
     def _make_output_dir(self, output_dir):
-        """Makes subdirectory in user-specified path and sets paths for CSV output."""
+        """
+        Make a subdirectory in the specified path for output and set paths for CSV output.
+
+        Args:
+            output_dir (Path): Output directory path.
+
+        """
         self.output_dir = output_dir / "chimp_output"
         self.class_csv_output_path = self.output_dir / "chimp_results.csv"
         logging.info(f"Making directory for output {self.output_dir}")
         self.output_dir.mkdir(exist_ok=True)
 
     def _load_model_dict(self, model_path):
+        """
+        Load the model dictionary from the given path.
+
+        Args:
+            model_path (Path): Path to the trained model.
+
+        Returns:
+            dict: Model dictionary.
+
+        """
         if self.device == "cpu":
             map_location = self.device
         else:
@@ -69,6 +122,16 @@ class ImageFolderClassifier:
         return torch.load(model_path, map_location=map_location)
 
     def _get_torch_device(self, device):
+        """
+        Get the torch device based on the specified device string.
+
+        Args:
+            device (str): Device string.
+
+        Returns:
+            torch.device: Torch device.
+
+        """
         if device == "cpu":
             device_str = device
         else:
@@ -76,7 +139,13 @@ class ImageFolderClassifier:
         return torch.device(device_str)
 
     def _predict_classes(self):
-        """Method that predicts image classes, stores outputs in a dictionary."""
+        """
+        Predict the image classes and store the outputs in a dictionary.
+
+        Returns:
+            dict: Dictionary containing the prediction results.
+
+        """
         s_max = nn.Softmax(dim=1)
         logging.info("Predicting image classes.")
         with torch.no_grad():
@@ -104,7 +173,10 @@ class ImageFolderClassifier:
         return dict(self.output_dict)
 
     def _output_csv(self):
-        """Converts data dictionary into a csv file for output and saves to disk."""
+        """
+        Convert the data dictionary into a CSV file for output and save it to disk.
+
+        """
         chimp_table = pd.DataFrame(data=self.output_dict)
         chimp_table.to_csv(self.class_csv_output_path, index=False)
         logging.info("Results output to: {}".format(self.class_csv_output_path))
